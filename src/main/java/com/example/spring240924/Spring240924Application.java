f171aad54f5e4364bcaa85c2d96da860780ca61b
koreaunnie <hyunjung1995@gmail.com>
2024-09-24 14:06:20 +0900

    first commit

---
+++ src/main/java/com/example/spring240924/Spring240924Application.java
@@ -0,0 +1,13 @@
+package com.example.spring240924;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring240924Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring240924Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring240924/Spring240924ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring240924;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring240924ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
