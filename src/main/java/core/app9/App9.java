c0b0040ef4067e0ebba307872a939380cb5ab840
koreaunnie <hyunjung1995@gmail.com>
2024-10-17 16:30:55 +0900

    2024.10.17 수업

--- src/main/java/core/app9/App9.java
+++ src/main/java/core/app9/App9.java
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App9 {
     public static void main(String[] args) {
         BeanFactory context = SpringApplication.run(App9.class, args);
-        Com1 b1 = context.getBean(Com1.class);
-        Com2 b2 = context.getBean(Com2.class);
+        Comp1 b1 = context.getBean(Comp1.class);
+        Comp2 b2 = context.getBean(Comp2.class);
 
-        System.out.println(b1.getCom2() == b2);
+        System.out.println(b1.getComp2() == b2);
     }
 
 }
