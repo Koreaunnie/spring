630fc49656edfae2397e8b6b9b59c4685a837ae3
koreaunnie <hyunjung1995@gmail.com>
2024-11-12 16:27:15 +0900

    고객, 거래처 조회

---
+++ src/main/java/com/example/spring240924/api/ApiController6.java
@@ -0,0 +1,30 @@
+package com.example.spring240924.api;
+
+import com.example.spring240924.dto.a6.Customer;
+import com.example.spring240924.dto.a6.Supplier;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/main6")
+@RequiredArgsConstructor
+public class ApiController6 {
+    private final Service6 service;
+
+    @GetMapping("sub1/{customerId}")
+    @ResponseBody
+    public Customer get(@PathVariable Integer customerId) {
+        return service.getCustomer(customerId);
+    }
+
+    @GetMapping("sub2/{supplierId}")
+    @ResponseBody
+    public Supplier getSupplier(@PathVariable Integer supplierId) {
+        return service.getSupplier(supplierId);
+    }
+
+}
---
+++ src/main/java/com/example/spring240924/api/Service6.java
@@ -0,0 +1,24 @@
+package com.example.spring240924.api;
+
+import com.example.spring240924.dto.a6.Customer;
+import com.example.spring240924.dto.a6.Supplier;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class Service6 {
+    private final Mapper6 mapper;
+
+    public Customer getCustomer(Integer customerId) {
+        return mapper.selectCustomerById(customerId);
+    }
+
+    public Supplier getSupplier(Integer supplierId) {
+
+        return mapper.selectSupplierById(supplierId);
+    }
+
+}
