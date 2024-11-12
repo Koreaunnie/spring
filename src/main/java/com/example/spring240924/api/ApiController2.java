f639f3f7f140934d8f76179e01742812dfb57c17
koreaunnie <hyunjung1995@gmail.com>
2024-11-12 10:37:18 +0900

    json을 javaBean(dto)로 받기

--- src/main/java/com/example/spring240924/api/ApiController2.java
+++ src/main/java/com/example/spring240924/api/ApiController2.java
@@ -3,13 +3,13 @@ package com.example.spring240924.api;
 import com.example.spring240924.dto.a2.Person;
 import com.example.spring240924.dto.a2.Book;
 import com.example.spring240924.dto.a2.Player;
+import com.example.spring240924.dto.a2.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/main2")
@@ -27,5 +27,25 @@ public class ApiController2 {
     @PostMapping("sub3")
     public void method3(@RequestBody Player player) {
         System.out.println(player);
+    }
+
+    @PostMapping("sub4")
+    public void method4(@RequestBody Product product) {
+        System.out.println(product);
+    }
+
+    @PostMapping("sub5")
+    public void method5(@RequestBody List<String> items) {
+        System.out.println(items);
+    }
+
+    @PostMapping("sub6")
+    public void method6(@RequestBody List<Person> peoples) {
+        System.out.println(peoples);
+    }
+
+    @PostMapping("sub7")
+    public void method7(@RequestBody List<Book> books) {
+        System.out.println(books);
     }
 }
