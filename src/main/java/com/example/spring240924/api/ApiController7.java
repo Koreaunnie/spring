6e6e01f65e294b11b2827a7af6fec159be0728e0
koreaunnie <hyunjung1995@gmail.com>
2024-11-13 10:14:54 +0900

    ResponseEntity<T> 의 T는 무시해도 됨

--- src/main/java/com/example/spring240924/api/ApiController7.java
+++ src/main/java/com/example/spring240924/api/ApiController7.java
@@ -1,5 +1,6 @@
 package com.example.spring240924.api;
 
+import com.example.spring240924.dto.a6.Customer;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -88,10 +89,34 @@ public class ApiController7 {
 
     // 405
     @GetMapping("sub12")
-    public ResponseEntity sub12() {
+    public ResponseEntity<Map<String, Object>> sub12() {
         ResponseEntity response =
                 ResponseEntity.status(405)
                         .body(Map.of("name", "kim", "age", "30"));
         return response;
+    }
+
+    @GetMapping("sub13")
+    public ResponseEntity<Customer> sub13() {
+        ResponseEntity response =
+                ResponseEntity.ok()
+                        .body(new Customer());
+        return response;
+    }
+
+    @GetMapping("sub14")
+    public ResponseEntity<Object> sub14() {
+        boolean good = true;
+        if (good) {
+            ResponseEntity response =
+                    ResponseEntity.ok()
+                            .body(new Customer());
+            return response;
+        } else {
+            ResponseEntity response =
+                    ResponseEntity.status(404)
+                            .body(Map.of("name", "kim", "age", "30"));
+            return response;
+        }
     }
 }
